212cec3122e229d191bee54835a8068aa521f7d0
teca-admin <grupoorbital.teca@gmail.com>
2026-05-04 13:48:04 -0400

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,11 @@
-import app from '../server.ts';
+import express from 'express';
+import dotenv from 'dotenv';
+import { sheetsApiRouter } from '../server/api/sheets.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use('/api/sheets', sheetsApiRouter);
+
 export default app;
